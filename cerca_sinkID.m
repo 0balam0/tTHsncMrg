72e0695caadca0624b443998a12b5f5d7f8b21b1
MatteoFCA <matteo.demarco@external.stellantis.com>
2022-08-05 15:11:26 +0200

    add comment cerca_sinkID

--- cerca_sinkID.m
+++ cerca_sinkID.m
@@ -1,5 +1,8 @@
 function idMin = cerca_sinkID(signalL, signalS)
-    
+% cerca id su signalL in cui dovrebbe partire signalS
+
+% traspone signalS se non è coerente con signalL (2 vettori riga/ 3 vettori
+% colonna)
     trs = sum(size(signalL(1:10)) == size(signalS(1:10)));
     if trs == 0
         signalS = signalS';
@@ -7,20 +10,22 @@ function idMin = cerca_sinkID(signalL, signalS)
 
     lenL = length(signalL);
     lenS = length(signalS);
-%     if lenL<=lenS
-%         idMin = 1;
-%     else
+
+ % per ogni punto id (da 1  ad lenL-lenS) di signalL calcola la differenza
+ % tra i due segnali.
     error = ones(size(signalL))*inf;
     for idStart=1:(lenL-lenS)
         error(idStart) = sum(abs(signalL(idStart:lenS+idStart-1) - signalS));
     end
-    [~, idMin] = min(error);
-    fprintf('id min trovato da funzione completa: %d con errore %.2f', idMin, error(idMin));
-        figure();
-        plot(1:1:length(signalL), error); hold on;
-        plot(idMin, error(idMin), 'o');
-%         figure();
-%         plot(1:1:length(signalL), signalL); hold on;
-%         plot((1:1:length(signalS))+idMin, signalS); hold on;
-%     end
+    [~, idMin] = min(error);    
+
+% plot dell'errore 
+%     fprintf('id min trovato da funzione completa: %d con errore %.2f', idMin, error(idMin));
+%     figure();
+%     plot(1:1:length(signalL), error); hold on;
+%     plot(idMin, error(idMin), 'o');
+% %         figure();
+% %         plot(1:1:length(signalL), signalL); hold on;
+% %         plot((1:1:length(signalS))+idMin, signalS); hold on;
+% %     end
 end
