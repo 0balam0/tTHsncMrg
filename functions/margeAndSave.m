c1ee31b966faa3fe98ecd8b06485559e9ef11a77
MatteoFCA <matteo.demarco@external.stellantis.com>
2022-08-09 00:00:49 +0200

    Gui update, solved save function

--- functions/margeAndSave.m
+++ functions/margeAndSave.m
@@ -5,23 +5,40 @@ function tTH1 = margeAndSave(risp, timeSnc, prefix2, tTH1, tTH2)
             switch risp
                 case 1 % Save duplicates with prefix se esistono duplicati con prefisso allora li unisce
                     names1 = fieldnames(tTH1);
+                    names1Wprf2 = {};
+                    for k=1:length(names1)
+                        d = tTH1.(names1{k}).d;
+                        s = strsplit(d, strcat(prefix2,':\\ '));
+                        if length(s)>1
+                            names1Wprf2{end+1} = names1{k};
+                        end
+                    end
+                    disp(names1Wprf2)
                     names2 = fieldnames(tTH2);
                     for i =1:length(names2)
                         name2 = names2{i};
-                        save_name = name2;
-                        if any(strcmp(names1, name2)) 
+                        if ~strcmp('time', name2)
+                            save_name = name2;
                             preName2 = strcat(prefix2, name2);
-                            save_name = preName2;
-                            if  ~any(strcmp(names1, preName2))
-                                tTH1.(preName2) = tTH2.(name2);
-                                tTH1.(preName2).v = tTH1.time.v *0;
+                            if any(strcmp(names1, name2)) 
+                                if any(strcmp(names1, preName2))
+                                    save_name = preName2;
+                                elseif  any(strcmp(names1Wprf2, name2))
+                                    save_name=name2;
+                                else
+                                    tTH1.(save_name) = tTH2.(name2);
+                                    tTH1.(save_name).v = tTH1.time.v *0;
+                                    tTH1.(save_name).d = strcat(prefix2,':\\ ',tTH1.(save_name).d);
+                                    save_name = name2;
+                                end
+                            else                            
+                                tTH1.(save_name) = tTH2.(name2);
+                                tTH1.(save_name).v = tTH1.time.v .*0;
+                                tTH1.(save_name).d = strcat(prefix2,':\\ ',tTH1.(save_name).d);
                             end
-                        else
-                            tTH1.(save_name) = tTH2.(name2);
-                            tTH1.(save_name).v = tTH1.time.v .*0;
-                        end
 
-                        tTH1.(save_name).v(idMin:idMin+length(tTH2.time.v)-1) = tTH2.(name2).v;
+                            tTH1.(save_name).v(idMin:idMin+length(tTH2.time.v)-1) = tTH2.(name2).v;
+                        end
                     end
                     
                 case 2 % Overwrite duplicates tTH2 on tTH1
